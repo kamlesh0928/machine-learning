974f4c48a444b9f8ac99afe01e1f6d359ca1b6a7
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2024-10-17 18:36:19 +0530

    Update Naive_Bayes_implementation_from_scratch.py

--- Naive Bayes/Naive_Bayes_implementation_from_scratch.py
+++ Naive Bayes/Naive_Bayes_implementation_from_scratch.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
 
 # Load the Wine dataset
 wine = load_wine()
@@ -10,8 +11,6 @@ y = wine.target
 
 print(X)
 print(y)
-
-from sklearn.model_selection import train_test_split
 
 # Split the dataset into training and test sets (80% training, 20% testing)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
