f704eefdb5577c2593f0b5f05438e741e04e545c
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2024-10-27 14:08:45 +0530

    Update PCA_sklearn.py

--- PCA/PCA_sklearn.py
+++ PCA/PCA_sklearn.py
@@ -5,6 +5,7 @@ import plotly.express as px
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+# This dataset is provided in this repository.
 # Update the path below to your local path where the dataset is located
 dataset = pd.read_csv("/content/drive/MyDrive/Digit Recognizer Dataset/train.csv")
 
